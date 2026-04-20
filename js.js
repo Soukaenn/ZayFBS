8b0aa263b00d826fc60bae0c1e2b84da21a67fbd
Soukaenn <soukainaennaour3@gmail.com>
2026-04-20 15:37:38 +0100

    new

---
+++ js.js
@@ -0,0 +1,21 @@
+
+//ex1
+const bouton = document.getElementById("btn");
+const message = document.getElementById("message");
+
+bouton.addEventListener("click", function() {
+    message.textContent = "Tu as cliqué sur le bouton !";
+});
+//ex2
+const text= document.getElementById("text");
+text.addEventListener("click", () =>{
+    text.style.color="green";
+});
+const id= document.getElementById("add");
+const liste= document.getElementById("list");
+
+id.addEventListener("click", () =>{ 
+liste.style.color
+} 
+
+)
